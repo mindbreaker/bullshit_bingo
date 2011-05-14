a73fe133cc6ae0a8d89bd94df3e7ca991d8e135e
Mindbreaker <tczernik.mmt-b2008@fh-salzburg.ac.at>
2011-05-14 17:08:32 +0200

    Play sound at 5 selected entries

--- bingo.js
+++ bingo.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
         dataType: 'jsonp',
         success: function(data, status, xhr ) {
             //console.log(data);
+			var count = 0;
             for (var key in data) {
                 var tmp = "";
 
@@ -19,8 +20,18 @@ $(document).ready(function() {
                     tmp += '<input type = "checkbox" name="'+ data[key][i].replace(/\s/g,"-") + '" class="custom" id="'+ data[key][i].replace(/\s/g,"-") + '"  /><label for="'+ data[key][i].replace(/\s/g,"-") + '">'+ data[key][i] + '</label>';
                 }
                 $("#firstlist").append('<li><a href="#show-'+key+'">'+ key + '</a></li>');
-				$("body").append('<div data-role="page" data-url="show-'+key+'"><div data-role="header"><h1>'+ key +'</h1></div><div data-role="content">'+tmp+'</div></div>');
+				$("body").append('<div data-role="page" data-url="show-'+key+'" id="show-'+key+'"><div data-role="header"><h1>'+ key +'</h1></div><div data-role="content">'+tmp+'</div></div>');
 				//$("#firstlist").append('<li><a href="#">'+ key + '</a><div data-role="fieldcontain">'+tmp + '</div></li>');
+				$("#show-"+key+" input[type='checkbox']").change( function(el) {
+					count++;
+					
+					// Bingo
+					if(count>=5) {
+						$("audio").get(0).play();
+						alert("Du hast gewonnen");
+						count=0;
+					}
+				});
             }
             $('ul').listview('refresh');
         }
