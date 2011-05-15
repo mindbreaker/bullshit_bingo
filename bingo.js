aadf19b119961e54f3e11ee2fcc0361263bb2d69
Mindbreaker <tczernik.mmt-b2008@fh-salzburg.ac.at>
2011-05-15 17:46:11 +0200

    Readme

--- bingo.js
+++ bingo.js
@@ -10,45 +10,44 @@ $(document).ready(function() {
     $.ajax({
         url: 'http://buzzwords.tladesignz.com/data.pl',
         dataType: 'jsonp',
-        success: function(data, status, xhr ) {
-            //console.log(data);
-			var count = 0;
+        success: function(data) {
+            var count = 0;
             for (var key in data) {
                 var tmp = "";
-				var selection = new Array();
-				for(j=0; j<5;j++)
-				{
-					var is_in_selection = true;
-					while(is_in_selection == true){
-						var tmp_index = Math.floor(Math.random()*(data[key].length));
-						if ($.inArray(tmp_index, selection) != -1) 
-						{
-							is_in_selection = true;
-						} 
-						else 
-						{
-							is_in_selection = false;
-							selection[j]= tmp_index;
-						}
-					}
-				}
+                var selection = new Array();
+                for(j=0; j<5;j++)
+                {
+                    var is_in_selection = true;
+                    while(is_in_selection == true){
+                        var tmp_index = Math.floor(Math.random()*(data[key].length));
+                        if ($.inArray(tmp_index, selection) != -1)
+                        {
+                            is_in_selection = true;
+                        }
+                        else
+                        {
+                            is_in_selection = false;
+                            selection[j]= tmp_index;
+                        }
+                    }
+                }
 
                 for(i = 0; i<5; i++) {
                     tmp += '<input type = "checkbox" name="'+ data[key][selection[i]].replace(/\s/g,"-") + '" class="custom" id="'+ data[key][selection[i]].replace(/\s/g,"-") + '"  /><label for="'+ data[key][selection[i]].replace(/\s/g,"-") + '">'+ data[key][selection[i]] + '</label>';
                 }
                 $("#firstlist").append('<li><a href="#show-'+key+'">'+ key + '</a></li>');
-				$("body").append('<div data-role="page" data-url="show-'+key+'" id="show-'+key+'"><div data-role="header"><h1>'+ key +'</h1></div><div data-role="content">'+tmp+'</div></div>');
-				//$("#firstlist").append('<li><a href="#">'+ key + '</a><div data-role="fieldcontain">'+tmp + '</div></li>');
-				$("#show-"+key+" input[type='checkbox']").change( function(el) {
-					count++;
+                $("body").append('<div data-role="page" data-url="show-'+key+'" id="show-'+key+'"><div data-role="header"><h1>'+ key +'</h1></div><div data-role="content">'+tmp+'</div></div>');
+                //$("#firstlist").append('<li><a href="#">'+ key + '</a><div data-role="fieldcontain">'+tmp + '</div></li>');
+                $("#show-"+key+" input[type='checkbox']").change( function(el) {
+                    count++;
 					
-					// Bingo
-					if(count>=5) {
-						$("audio").get(0).play();
-						alert("Du hast gewonnen");
-						count=0;
-					}
-				});
+                    // Bingo
+                    if(count>=5) {
+                        $("audio").get(0).play();
+                        alert("Du hast gewonnen");
+                        count=0;
+                    }
+                });
             }
             $('ul').listview('refresh');
         }
